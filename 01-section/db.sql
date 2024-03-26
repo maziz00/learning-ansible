6c7f0afe6fe6e8bbf055ae568ad0403c667548f8
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-03-26 10:33:49 +0400

    feat(02-section):host & group vars

--- 01-section/db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABSE employee_db;
-CREATE USER 'db_user'@'localhost' IDENTIFIED BY 'Passw0rd';
-GRANT ALL ON employee_db.* TO 'db_user'@'localhost';
-USE employee_db;
-CREATE TABLE employees (name VARCHAR(20));
