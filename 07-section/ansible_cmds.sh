f0fb608a85cca9bb9319ea22bdd20b6599fc4c04
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-03-27 12:07:57 +0400

    feat(07-section): Jinja2 ip address playbook

--- 07-section/ansible_cmds.sh
+++ 07-section/ansible_cmds.sh
@@ -3,8 +3,6 @@
 
 # ansible-playbook playbook-debug.yml
 
-ansible-playbook -i "localhost" playbook-debug.yml
-
-ansible-playbook -i "localhost" playbook-random.yml
+ansible-playbook -i "localhost" playbook-*.yml
 
 ansible-playbook playbook.yml -i inventory
