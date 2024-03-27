22d162ba9154b020bbda6e3b950db6133e0c311e
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-03-27 12:04:44 +0400

    feat(07-section): Jinja2 random file

--- 07-section/ansible_cmds.sh
+++ 07-section/ansible_cmds.sh
@@ -3,6 +3,8 @@
 
 # ansible-playbook playbook-debug.yml
 
-ansible-playbook -i "localhost" playbook-local.yml
+ansible-playbook -i "localhost" playbook-debug.yml
+
+ansible-playbook -i "localhost" playbook-random.yml
 
 ansible-playbook playbook.yml -i inventory
